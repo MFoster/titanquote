7f73c76c09de4f4d187c6bde14dea40c33898207
matt foster <mattfoster01@gmail.com>
2015-03-04 12:14:55 -0500

    adds settings file with database.active boolean.  Used to determine which service to pull quotes from.  Refactors DefaultController to use this configuration property.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,7 +10,12 @@ class DefaultController extends ControllerBase {
    * {@inheritdoc}
    */
   public function quoteAction() {
-  	$service = \Drupal::service('titan.quote.database');
+  	$config = \Drupal::config('titanquote.settings');
+
+  	$db_active = $config->get('database.active');
+
+    $service = ($db_active ? \Drupal::service('titan.quote.database') : \Drupal::service('titan.quote.local'));
+
   	$quote = $service->getRandom();
 		return array(
 			'#type' => 'markup',
