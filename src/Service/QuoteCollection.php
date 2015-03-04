0489432e95e147440e3939f04628f0dc9c766cb5
matt foster <mattfoster01@gmail.com>
2015-03-03 20:15:05 -0500

    adds service definition file for local quote collection. Adds the QuoteCollection service class to provide the array of quote objects

---
+++ src/Service/QuoteCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\titanquote\Service;
+
+class QuoteCollection{
+
+	protected $quotes = array();
+
+	public function setQuotes($quotes){
+		$this->quotes = $quotes;
+	}
+
+	public function getRandom() {
+		$max = count($this->quotes) - 1;
+
+		$index = floor(rand(0, $max));
+
+		return $this->quotes[$index];
+  }
+}
